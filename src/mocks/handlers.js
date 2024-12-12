7f9a4ea8070a2ec833c1e7bc351f9f8892dd625d
sparrro <116090214+sparrro@users.noreply.github.com>
2024-12-12 02:26:54 +0100

    mocks

---
+++ src/mocks/handlers.js
@@ -0,0 +1,24 @@
+import { http, HttpResponse } from "msw";
+
+export const handlers = [
+    http.get("https://h5jbtjv6if.execute-api.eu-north-1.amazonaws.com/confirmation", () => {
+        const data = JSON.parse(sessionStorage.getItem("confirmation"));
+        return HttpResponse.json(data);
+    }),
+    http.post("https://h5jbtjv6if.execute-api.eu-north-1.amazonaws.com/confirmation", async ({request}) => {
+        const body = await request.json();
+        const {when, lanes, people, shoes} = body;
+        const price = parseInt(people)*120+parseInt(lanes)*100;
+        const confirmation = {
+            id: 128376,
+            when: when,
+            lanes: lanes,
+            people: people,
+            shoes: shoes,
+            price: price,
+            active: true
+        }
+        sessionStorage.setItem("confirmation", JSON.stringify(confirmation));
+        return HttpResponse.json(confirmation);
+    }),
+]
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setup.js",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "json-summary"],
+      reportsDirectory: "./coverage",
+      all: true,
+      exclude: ["./src/main.jsx", "vite.config.js"],
+    }
+  }
 })
